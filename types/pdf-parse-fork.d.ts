1ace74601ca6a4c244405aad16988215800d5671
Stefan-Groen <stefangroen.gs@gmail.com>
2025-11-12 21:38:28 +0100

    add typescript declarations for pdf-parse-fork

---
+++ types/pdf-parse-fork.d.ts
@@ -0,0 +1,26 @@
+declare module 'pdf-parse-fork' {
+  interface PDFInfo {
+    PDFFormatVersion: string
+    IsAcroFormPresent: boolean
+    IsXFAPresent: boolean
+    [key: string]: any
+  }
+
+  interface PDFMetadata {
+    _metadata: any
+    [key: string]: any
+  }
+
+  interface PDFData {
+    numpages: number
+    numrender: number
+    info: PDFInfo
+    metadata: PDFMetadata
+    text: string
+    version: string
+  }
+
+  function pdfParse(buffer: Buffer): Promise<PDFData>
+
+  export default pdfParse
+}
